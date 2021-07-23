0664410b5bd74a78ed67e21d67290882aaaecb1b
Den <wolf-den@tut.by>
2021-07-24 00:36:40 +0300

    feat: request formdata and json on server php

--- js/script.js
+++ js/script.js
@@ -124,7 +124,7 @@ window.addEventListener('DOMContentLoaded', () => {
     btn.addEventListener('click', showModal);
   });
 
-  // const modalTimerId = setTimeout(showModal, 15000);
+  const modalTimerId = setTimeout(showModal, 15000);
 
   const sheckFinalPage = () => {
     if (
@@ -200,4 +200,58 @@ window.addEventListener('DOMContentLoaded', () => {
     21,
     '.menu .container'
   ).render();
+
+  // Forms
+  const forms = document.querySelectorAll('form');
+
+  const message = {
+    loading: 'Загрузка',
+    success: 'Спасибо! Скоро мы с вами свяжимся',
+    failure: 'Что-то пошло не так...'
+  }
+
+  forms.forEach(item => {
+    postData(item)
+  })
+  
+
+  function postData(form) {
+    form.addEventListener('submit', (e) => {
+      e.preventDefault();
+  
+      const statusMessage = document.createElement('div');
+      statusMessage.classList.add('status');
+      statusMessage.textContent = message.loading;
+      form.append(statusMessage)
+  
+      const request = new XMLHttpRequest();
+      request.open('POST', 'server.php');
+  
+      request.setRequestHeader('Content-type', 'application/json');
+      const formData = new FormData(form);
+
+      // в JSON
+      const object = {}
+      formData.forEach((value, key) => {
+        object[key] = value
+      });
+      const json = JSON.stringify(object) 
+  
+      // request.send(formData)
+      request.send(json);
+  
+      request.addEventListener('load', () => {
+        if (request.status === 200) {
+          console.log(request.response)
+          statusMessage.textContent = message.success
+          form.reset();
+          setTimeout(() => {
+            statusMessage.remove()
+          },2000)
+        } else {
+          statusMessage.textContent = message.failure
+        }
+      })
+    })
+  }
 });
